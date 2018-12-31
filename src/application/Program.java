f1387624194fc9344caf13db518dca01f867fc4a
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2018-12-31 15:46:18 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		Seller obj2 = new Seller(1, "Bob", "bob@gmail.com", sdf.parse("12/04/1987"), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println(obj2);
 	}
