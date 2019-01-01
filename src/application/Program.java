db7782060bdb1c578a69ea986de12002d476f435
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-01 18:48:50 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -41,7 +44,7 @@ public class Program {
 		System.out.println();
 
 		System.out.println("::::::::::::::: TEST 4: seller insert :::::::::::::::");
-		Seller newSeller = new Seller(null, "Greg", "greg@gemail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(null, "Silver Mello", "silver@gemail.com", new Date(), 4100.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id: " + newSeller.getId());
 		
@@ -53,7 +56,17 @@ public class Program {
 		seller.setEmail("sandra@gemail.com");
 		sellerDao.update(seller);
 		System.out.println("Updated! completed in: " + seller.getId());
+		
+		System.out.println();
 
+		System.out.println("::::::::::::::: TEST 6: seller delete :::::::::::::::");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deleted! completed");
+		
+
+		sc.close();
 	}
 
 }
